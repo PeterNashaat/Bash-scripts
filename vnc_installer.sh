f8b7c36f8ee54cd7ac4b0042c48d83a6bc40da04
PeterNashaat <peter@codescalers.com>
2020-05-05 15:52:07 +0200

    Update vnc_installer.sh

--- vnc_installer.sh
+++ vnc_installer.sh
@@ -13,3 +13,16 @@ EOF
 
 chmod +x ~/.vnc/xstartup
 vncserver
+
+apt install vim firefox -y
+
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb http://download.virtualbox.org/virtualbox/debian bionic contrib"
+
+sudo apt update
+sudo apt install virtualbox-6.1 -y
+wget https://download.virtualbox.org/virtualbox/6.1.6/Oracle_VM_VirtualBox_Extension_Pack-6.1.6.vbox-extpack
+
+echo " TO INSTALL EXTENTION PACK PLEASE RUN THIS COMMAND"
+echo " VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-*.vbox-extpack "
